1471614df0737d5760d566ee7617f367449c4d3c
brelance <brelance82@gmail.com>
2025-01-20 13:20:48 +0800

    infra: add RPC state server

---
+++ state/rpc/server/service.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"context"
+
+	"github.com/brelance/plato/common/logger"
+)
+
+// RPC
+
+const (
+	CancelConnCmd = 1
+	SendMsgCmd    = 2
+)
+
+type CmdContext struct {
+	Ctx      *context.Context
+	Cmd      int32
+	Endpoint string
+	ConnID   uint64
+	payload  []byte
+}
+
+type Service struct {
+	CmdChannel chan *CmdContext
+}
+
+func (service *Service) CancelConn(ctx context.Context, sr *StateRequest) (*StateResponse, error) {
+	c := context.TODO()
+	cmd := &CmdContext{
+		Ctx:      &c,
+		Cmd:      CancelConnCmd,
+		Endpoint: sr.GetEndpoint(),
+		ConnID:   sr.GetConnID(),
+		payload:  sr.GetData(),
+	}
+	service.CmdChannel <- cmd
+	logger.Logger.
+		Debug().
+		Msgf("Cancel connection: endpoint=%s, connID=%d, chanLen=%d", cmd.Endpoint, cmd.ConnID, len(service.CmdChannel))
+	return &StateResponse{
+		Code: 0,
+		Msg:  "success",
+	}, nil
+}
+func (service *Service) SendMsg(ctx context.Context, sr *StateRequest) (*StateResponse, error) {
+	c := context.TODO()
+	cmd := &CmdContext{
+		Ctx:      &c,
+		Cmd:      SendMsgCmd,
+		Endpoint: sr.GetEndpoint(),
+		ConnID:   sr.GetConnID(),
+		payload:  sr.GetData(),
+	}
+	service.CmdChannel <- cmd
+	logger.Logger.
+		Debug().
+		Msgf("Send message: endpoint=%s, connID=%d, chanLen=%d", cmd.Endpoint, cmd.ConnID, len(service.CmdChannel))
+	return &StateResponse{
+		Code: 0,
+		Msg:  "success",
+	}, nil
+}
